36778459b7751b157b7fec48cc7d3e5e2b39a04d
Chunyip <chunyip6599@gmail.com>
2021-02-10 01:40:31 +0800

    Added Payment Page

--- Clinic Management System/frmCollectionDetails.vb
+++ Clinic Management System/frmCollectionDetails.vb
@@ -4,6 +4,8 @@
 
     Private Sub frmCollectionDetails_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+
+        ''dgv.Columns.Add("Unit Total", "Unit Total (RM)")
     End Sub
 
     Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
@@ -20,6 +22,8 @@
     End Sub
 
     Private Sub frmCollectionDetails_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        chkCollectionDone.Checked = False
+
         Dim db As New CMSDatabaseDataContext()
         Dim p As Patient = db.Patients.FirstOrDefault(Function(o) o.PatientId = SelectedId)
 
@@ -42,8 +46,37 @@
         Dim rs = From m In db.Inventories
                  Join l In db.Medicals On l.ItemId Equals m.ItemId
                  Where l.PatientId = SelectedId
-                 Select m.ItemName, l.AmountTaken, l.ItemId
+                 Select l.ItemId, m.ItemName, m.ItemDescription, m.UnitPrice, l.AmountTaken
+
+        ''Dim table As New DataTable("table")
+
+        ''table.Columns.Add("Unit Total", Type.GetType("System.Double"))
 
         dgv.DataSource = rs
+
+        dgv.Columns(0).HeaderCell.Value = "Item ID"
+        dgv.Columns(1).HeaderCell.Value = "Item Name"
+        dgv.Columns(2).HeaderCell.Value = "Item Description"
+        dgv.Columns(3).HeaderCell.Value = "Unit Price (RM)"
+        dgv.Columns(4).HeaderCell.Value = "Quantity"
+        dgv.Columns.Add("Unit Total", "Unit Total (RM)")
+
+        For i As Integer = 0 To dgv.Rows.Count - 1
+            Dim unitPrice As Double = dgv.Rows(i).Cells(3).Value
+            Dim quantity As Integer = dgv.Rows(i).Cells(4).Value
+
+            Dim unitTotal As Double = unitPrice * quantity
+
+            dgv.Rows(i).Cells(5).Value = unitTotal.ToString("N2")
+        Next
+    End Sub
+
+    Private Sub frmCollectionDetails_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        dgv.Columns.RemoveAt(5)
+    End Sub
+
+    Private Sub btnProceedCheckout_Click(sender As Object, e As EventArgs) Handles btnProceedCheckout.Click
+        frmMakePayment.SelectedId = SelectedId
+        frmMakePayment.ShowDialog(Me)
     End Sub
 End Class
